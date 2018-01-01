fe59ab3dffbc65e8839bb74cee17547cf2e6f458
Hoon Lee <yl.hoony@gmail.com>
2018-01-01 09:05:52 +0000

    Done.

---
+++ db/migrate/20180101082732_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.integer :release_date
+      t.string :director
+      t.string :lead
+      t.boolean :in_theaters
+
+      t.timestamps null: false
+    end
+  end
+end
