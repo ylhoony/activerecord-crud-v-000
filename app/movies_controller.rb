53f2a4b368317d9e08296902104d576448f1035f
Hoon Lee <yl.hoony@gmail.com>
2018-01-01 08:47:47 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,8 +24,8 @@ end
 
 def can_be_created_in_a_block
   Movie.create do |m|
-    m.title: "Home Alone"
-    m.release_date: 1990
+    m.title = "Home Alone"
+    m.release_date = 1990
   end
 end
 
